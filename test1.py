b18faedbdfffd54a93e570a6b0e7de460aedd61a
Qizong CB <qizong1102@gmail.com>
2019-07-10 13:05:21 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,42 @@
+import azure.cosmos.cosmos_client as cosmos_client
+
+
+config = {
+    'ENDPOINT': 'https://localhost:8081',
+    'PRIMARYKEY': 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
+    'DATABASE': 'CosmosDatabase',
+    'CONTAINER': 'CosmosContainer'
+}
+
+# Initialize the Cosmos client
+client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
+                                    'masterKey': config['PRIMARYKEY']})
+
+# Create a database
+#db = client.CreateDatabase({'id': config['DATABASE']})
+
+# Create container options
+options = {
+    'offerThroughput': 400
+}
+
+container_definition = {
+    'id': config['CONTAINER']
+}
+
+# Create a container
+container = client.CreateContainer(db['_self'], container_definition, options)
+
+# Create and add some items to the container
+
+
+# Query these items in SQL
+query = {'query': 'SELECT * FROM server s'}
+
+options = {}
+options['enableCrossPartitionQuery'] = True
+options['maxItemCount'] = 2
+
+result_iterable = client.QueryItems(container['_self'], query, options)
+for item in iter(result_iterable):
+    print(item['message'])
